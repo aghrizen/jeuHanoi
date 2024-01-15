4007f655c6075351ae841b737660cc39fcc51934
Salma <137169494+aghrizen@users.noreply.github.com>
2024-01-15 14:45:46 +0100

    Update main.cpp
    Signed-off-by: Salma <137169494+aghrizen@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-#include "ToursDeHnoi.hpp"
+#include "ToursHanoi.hpp"
+#include <list>
+using namespace std;
 
 int main() {
   int nombreDisques; //nombre des disques pour le jeu
+  list<int> myliste;
 
   std::cout << "Veuillez choisir un numéro des diques pour commencer le jeu: ";
   std::cin >> nombreDisques;
 
-  ToursDeHanoi game(nombreDisques);
+  ToursHanoi.start(nombreDisques);
   game.resoudre();
 
   return 0;
