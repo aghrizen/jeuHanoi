1aeeecd42956b88f129c6352396bee64ecfbd49f
Salma <137169494+aghrizen@users.noreply.github.com>
2024-01-15 14:00:31 +0100

    Update Tours.hpp
    Signed-off-by: Salma <137169494+aghrizen@users.noreply.github.com>

--- Tours.hpp
+++ Tours.hpp
@@ -1,1 +1,32 @@
-n
+// pour éviter la double inclusion:
+#ifndef TOURSHANOI_TOUR_CLASS
+#define TOURSHANOI_TOUR_CLASS
+#include <string>
+using namespace std;
+
+/*
+ *Définition du Tour avec ses disques
+ */
+
+#include "Disque.hpp"
+
+const int MAX_DISQUE = ; // Le nombre max des disques pour le jeu 
+class Tour {
+public: 
+  Tour();
+  Tour(int n, bool vide);
+  Disque getDisque(int k) const;
+  int sommet() const;
+  void push(const Disque& d);
+  void pop();
+  vid assign(int taille, bool vide);
+private:
+  // Le pilier
+  Disque m_pilier[MAX_DISQUE];
+  // L'indice de notre sommet
+  int m_top; 
+  // La dimension du socle
+  int m_dim; 
+};
+
+#include "Tour.cpp"
