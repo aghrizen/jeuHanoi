56a9f4f0a7319d154be4c909c708f70bdf45b37c
Salma <137169494+aghrizen@users.noreply.github.com>
2024-01-20 18:55:24 +0100

    Update jeuHanoi.hpp
    Signed-off-by: Salma <137169494+aghrizen@users.noreply.github.com>

--- jeuHanoi.hpp
+++ jeuHanoi.hpp
@@ -1,12 +1,16 @@
-#ifndef TOURSHANOI_CLASS
-#define TOURSHANOI_CLASS
+/*
+ ************************************************************************************
+ * Authors: salma.aghrizen@etu.univ-cotedazur.fr, bastien.briat@etu.univ-cotedazur.fr
+ ************************************************************************************
+ *
+ * Ce script contient la déclaration de la classe ToursHanoi pour décrir le déroulement du jeu.
+ */
+#ifndef JEUHANOI_CLASS
+#define JEUHANOI_CLASS
 #include <iostream>
 #include <string>
 using namespace std;
 
-/**
-  Les Tours de Hanoi
-*/
 #include "Tour.hpp"
 const int NMAXTOURS = 3;
 class ToursHanoi
