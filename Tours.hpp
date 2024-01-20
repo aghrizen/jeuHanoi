73594ecd820b9c12d368e4e021706d25360b33fa
Rajotah1 <145575676+Rajotah1@users.noreply.github.com>
2024-01-20 18:36:55 +0100

    Update Tours.hpp
    Signed-off-by: Rajotah1 <145575676+Rajotah1@users.noreply.github.com>

--- Tours.hpp
+++ Tours.hpp
@@ -18,7 +18,7 @@ public:
   Tour(int TowerTag, int n);
   int TourTag;
   bool MoveDisk(Tour &TourDestination);
-  bool AddDisk(Disque &Disque);
+  bool AddDisk(Disque Disque);
   void ShowDiskList();
 
 private:
