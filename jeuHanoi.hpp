df383bed02a112971c0cc338bae6c9446d95e135
Rajotah1 <145575676+Rajotah1@users.noreply.github.com>
2024-01-24 21:59:43 +0100

    Update jeuHanoi.hpp
    Signed-off-by: Rajotah1 <145575676+Rajotah1@users.noreply.github.com>

--- jeuHanoi.hpp
+++ jeuHanoi.hpp
@@ -5,28 +5,34 @@
  *
  * Ce script contient la déclaration de la classe ToursHanoi pour décrir le déroulement du jeu.
  */
+
 #ifndef JEUHANOI_CLASS
 #define JEUHANOI_CLASS
 #include <iostream>
 #include <string>
+#include "Tours.hpp"
 using namespace std;
 
-#include "Tour.hpp"
+
+const int T_MAX = 3; // le nombre MAX des tours
 const int T_MAX = 3; // le nombre MAX des tours
 class ToursHanoi
 {
-  public:
-    explicit ToursHanoi(int n);
-    void afficherTours() const;
-    void jouer();
-  private:
-    void deplacer(int o, int d);
-    void hanoi(int n, int orig, int dest, int inter);
-    void initialiser();
-    
-    HTour m_tours[T_MAX]; // les tours
-    int m_hauteur; // nombre de disques
-    
-    int m_ndeplacement; // nombre de deplacements
+public:
+	explicit ToursHanoi(int n);
+	void afficherTours() const;
+	void jouer();
+	void resoudre();
+private:
+	bool deplacer(int o, int d);
+	bool checkWin();
+	void hanoi(int n, int orig, int dest, int inter);
+	void initialiser();
+
+
+	Tour* m_tours[T_MAX]; // les tours
+	int m_hauteur; // nombre de disques
+
+	int m_deplacement; // nombre de deplacements
 };
 #endif
