d9d966564a030147ca50feb2849d46c71a618666
Rajotah1 <145575676+Rajotah1@users.noreply.github.com>
2024-01-24 22:01:21 +0100

    Update Disque.hpp
    Signed-off-by: Rajotah1 <145575676+Rajotah1@users.noreply.github.com>

--- Disque.hpp
+++ Disque.hpp
@@ -1,4 +1,4 @@
-/* 
+/*
  ************************************************************************************
  * Authors: salma.aghrizen@etu.univ-cotedazur.fr, bastien.briat@etu.univ-cotedazur.fr
  ************************************************************************************
@@ -15,23 +15,25 @@ using namespace std;
 
 // La déclaration de la classe:
 class Disque {
-// La déclarations des éléments publiques:
+	// La déclarations des éléments publiques:
 public:
-  // La déclaration du constructeur:
-  Disque();
-  /* La déclaration du constructeur avec ces paramètres
-   * s: la taille
-   * t: la taille du jeu
-   */
-  Disque(int s, int t);
-  int getTaille() const;
-  // pour retourner une présentation sous forme de chaîne du disque
-  string toString() const;
-// La déclaration des éléments privés:
+	// La déclaration du constructeur:
+	Disque();
+	/* La déclaration du constructeur avec ces paramètres
+	 * s: la taille
+	 * t: la taille du jeu
+	 */
+	Disque(int s, int t);
+	int getTaille();
+	// pour retourner une présentation sous forme de chaîne du disque
+	string toString();
+	// La déclaration des éléments privés:
 private:
-  // la taille de notre disque
-  int m_taille;
-  // la taille de notre jeu
-  int m_jeusize;
+	// la taille de notre disque
+	int m_taille;
+	// la taille de notre jeu
+	int m_jeusize;
 };
+
+
 #endif
