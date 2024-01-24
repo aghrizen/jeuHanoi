e34a5de26ca5d02462d4804be0b8cdce76428363
Rajotah1 <145575676+Rajotah1@users.noreply.github.com>
2024-01-24 22:01:00 +0100

    Update Disque.cpp
    Signed-off-by: Rajotah1 <145575676+Rajotah1@users.noreply.github.com>

--- Disque.cpp
+++ Disque.cpp
@@ -7,40 +7,40 @@
 
 #include "Disque.hpp"
 
-// Un constructeur par défaut.
-Disque::Disque(): m_taille(0), m_szjeu(1) {}
+ // Un constructeur par défaut.
+Disque::Disque() : m_taille(0), m_jeusize(1) {}
 
 /*
  * Un constructeur normal avec les paramètres du disque:
- *   s = la taille du jeu
- *   t = la taille du disque 
- */ 
-Disque::Disque(int s, int t): m_taille(t), m_szjeu(s) {}
+ *   t = la taille du jeu
+ *   s = la taille du disque
+ */
+Disque::Disque(int s, int t) : m_taille(s), m_jeusize(t) {}
 
 /*
  * Fonction getTaille(): fonction qui retourne la taille de disque.
  * Résultat: entier représentant la taille du disque.
  */
 
-int Disque::getTaille() const
+int Disque::getTaille()
 {
-  return m_taille;
+	return m_taille;
 }
 
 
 /*
- * toString(): Fonction qui  utilise la taille du disque pour déterminer le nombre d'espaces avant 
+ * toString(): Fonction qui  utilise la taille du disque pour déterminer le nombre d'espaces avant
  * et après la représentation du disque, de sorte que le disque semble centré dans le jeu
  * Résultat: elle retourne le disque en caractères.
  */
 
-string Disque::toString() const
+string Disque::toString()
 {
-  // Si la taille du disque est 0, la variable nespaces est égale (la taille du jeu)-1, sinon, 
-  // est égale (la taille du jeu)- taille du disque.
-  int nespaces = (getTaille() == 0 ? m_szjeu-1U : m_szjeu-getTaille());
-  return string(nespaces, ' ')
-       + (getTaille() == 0 ? "|" : string(2*getTaille()-1, '*'))
-       + string(nespaces, ' ')
-       + " ";
+	// Si la taille du disque est 0, la variable nespaces est égale (la taille du jeu)-1, sinon, 
+	// est égale (la taille du jeu)- taille du disque.
+	int nespaces = (getTaille() == 0 ? m_jeusize - 1U : m_jeusize - getTaille());
+	return string(nespaces, ' ')
+		+ (getTaille() == 0 ? "|" : string(2 * getTaille() - 1, '*'))
+		+ string(nespaces, ' ')
+		+ " ";
 }
