2422df564e62c279f0d1319bb134f96a609820b9
Rajotah1 <145575676+Rajotah1@users.noreply.github.com>
2024-01-28 17:59:40 +0100

    Update jeuHanoi.hpp
    Signed-off-by: Rajotah1 <145575676+Rajotah1@users.noreply.github.com>

--- jeuHanoi.hpp
+++ jeuHanoi.hpp
@@ -1,37 +1,38 @@
-/*
- ************************************************************************************
- * Authors: salma.aghrizen@etu.univ-cotedazur.fr, bastien.briat@etu.univ-cotedazur.fr
- ************************************************************************************
- *
- * Ce script contient la déclaration de la classe ToursHanoi pour décrir le déroulement du jeu.
- */
-
-#ifndef JEUHANOI_CLASS
-#define JEUHANOI_CLASS
-#include <iostream>
-#include <string>
-#include "Tours.hpp"
-using namespace std;
-
-
 const int T_MAX = 3; // le nombre MAX des tours
 class ToursHanoi
 {
 public:
-	explicit ToursHanoi(int n);
-	void afficherTours() const;
 	void jouer();
 	void resoudre();
+	explicit ToursHanoi(int n);
 private:
 	bool deplacer(int o, int d);
 	bool checkWin();
 	void hanoi(int n, int orig, int dest, int inter);
+	void afficherTours();
+	void afficherToursNew();
 	void initialiser();
+	void DrawDisque(int SizeCurrentDisque, int indexTower, int indexDisque, int MinimumXSize, int MaximumXSize, int SizeY, int CurentTowerCenterX);
+	void CreateRect(int posX, int posY, int sizeX, int sizeY, int Color[3]);
+	unsigned char InitSDL(SDL_Window **window);
 
+	const int SIZE_X_GAME_BOARD = 1536/2;
+	const int SIZE_Y_GAME_BOARD = 864/2;
 
+	enum direction
+	{
+		LEFT = 1,
+		DOWN = 2,
+		UP = 3,
+		RIGHT = 4,
+		ENTER = 5,
+
+	};
+
+	SDL_Window *window = NULL;
+	SDL_Renderer *myRenderer;
 	Tour* m_tours[T_MAX]; // les tours
 	int m_hauteur; // nombre de disques
 
 	int m_deplacement; // nombre de deplacements
 };
-#endif
